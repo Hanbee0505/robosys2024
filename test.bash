3c4015de28dad741649e61988acae7b9ed2080f5
Hanbee Choi <banbee016002@gmail.com>
2024-11-30 13:55:15 +0900

    Rename plus_stdin to plus and add test.bash

--- test.bash
+++ test.bash
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 ng () {
-	echo ${1}行目が違うよ
+        echo ${1}行目が違うよ  #$1はngの1番目の引数
 	res=1
 }
 
 res=0
+a=山田
+[ "$a" = 上田 ] || ng "$LINENO"  # LINENOは，この行の行番号の入る変数
+[ "$a" = 山田 ] || ng "$LINENO"  # ngに第一引数として$LINENOを付与
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
-
-[ "${res}" = 0 ] && echo OK
-exit $res
+exit $res     # このシェルスクリプトの終了ステータスを返して終了
