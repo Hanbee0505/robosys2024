db6e67656eaad358ae70a2dc3bbf95dd0959529a
Hanbee0505 <banbee016002@gmail.com>
2024-11-30 18:16:41 +0900

    Update test.bash

--- test.bash
+++ test.bash
@@ -1,13 +1,27 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2024 Hanbee Choi
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-        echo ${1}行目が違うよ  #$1はngの1番目の引数
+	echo ${1}行目が違うよ
 	res=1
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"  # LINENOは，この行の行番号の入る変数
-[ "$a" = 山田 ] || ng "$LINENO"  # ngに第一引数として$LINENOを付与
 
-exit $res     # このシェルスクリプトの終了ステータスを返して終了
+### 正常な出力 ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] ||  ng "$LINENO"
+
+### 変な出力 ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] 	  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] 	  || ng "$LINENO"
+[ "${out}" = "" ] 	  || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
